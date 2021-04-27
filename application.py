29507c588c4050b8591291157ecd1c970a9f5f94
acholyn <amandaholyn@gmail.com>
2021-04-27 17:17:30 +0100

    updated

---
+++ application.py
@@ -0,0 +1,26 @@
+def create_app():
+    application = Flask(__name__)
+    #FontAwesome(application)
+    application.config['SECRET_KEY'] = 'change this unsecure key' #need for search
+    application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+    return application
+
+
+application = create_app()
+
+# define action for home page
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+
+# No caching at all for API endpoints.
+@application.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+    return response
+
+application.run(debug=True)
