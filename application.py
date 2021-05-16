a4b7a4195652c0d1d56582d0c4a206736782e03c
acholyn <amandaholyn@gmail.com>
2021-05-16 21:34:47 +0100

    remove hardcoding

--- application.py
+++ application.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, url_for, redirect, request 
+# imports for online file reading
+import csv
+import urllib.request
+import shutil
+import tempfile
+
+import pandas
+from flask import Flask, redirect, render_template, request, url_for
 #from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+# imports for search
+from wtforms import Form, SelectField, StringField, SubmitField
+
 
 def create_app():
     application = Flask(__name__)
     #FontAwesome(application)
     application.config['SECRET_KEY'] = 'change this unsecure key' #need for search
     application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
-
+    
     return application
 
 
@@ -18,7 +27,41 @@ application = create_app()
 # define action for home page
 @application.route('/')
 def index():
-    return render_template('index.html')
+
+# #searchform actions
+#     class searchform(Form):
+#         choices = [('term', 'term'),
+#                 ('type', 'type'),
+#                 ('description', 'description')]
+#         select = SelectField('search:', choices=choices)
+#         search = StringField('')
+
+#     # search = searchform(request.form)
+#     # if request.method == 'post':
+#     #         return search_results(search)
+
+#     results = []
+#     search_string = search.data['search']
+
+# definitions
+
+    with urllib.request.urlopen('https://docs.google.com/spreadsheets/d/e/2PACX-1vS8dVMITvA6q77MTBe4SShI9Q85G4A_3Y5C8xx7n-BLinoxOW0y7zI59qoLxgwQ0Aql-I_MBye1UY6G/pub?gid=0&single=true&output=tsv') as response:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            shutil.copyfileobj(response, tmp_file)
+
+    with open(tmp_file.name) as file:
+        coraldefs=pandas.read_csv(file, sep="\t", keep_default_na=False)
+        term=coraldefs.TERM
+        role=coraldefs.ROLE 
+        definition=coraldefs.DEFINITION 
+        referrals=coraldefs.REFERRALS
+        length=len(coraldefs)
+        print(length)
+
+
+    return render_template('index.html', coraldefs=coraldefs, term=term, role=role, definition=definition, referrals=referrals)
+
+
 
 # define action for contributor page
 @application.route('/contributors')
