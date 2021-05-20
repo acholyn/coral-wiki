929088b92105a03addf602fcb5bf9e14a950cfe6
acholyn <amandaholyn@gmail.com>
2021-05-20 02:16:26 +0100

    hyperlinks working

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 # imports for online file reading
 import csv
-import urllib.request
 import shutil
 import tempfile
+import urllib.request
+import re
 
 import pandas
 from flask import Flask, redirect, render_template, request, url_for
@@ -12,7 +13,7 @@ from flask_wtf import FlaskForm
 # imports for search
 from wtforms import Form, SelectField, StringField, SubmitField
 
-
+# set the app up
 def create_app():
     application = Flask(__name__)
     #FontAwesome(application)
@@ -46,17 +47,24 @@ def index():
 # definitions
 
     with urllib.request.urlopen('https://docs.google.com/spreadsheets/d/e/2PACX-1vS8dVMITvA6q77MTBe4SShI9Q85G4A_3Y5C8xx7n-BLinoxOW0y7zI59qoLxgwQ0Aql-I_MBye1UY6G/pub?gid=0&single=true&output=tsv') as response:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        with tempfile.NamedTemporaryFile(delete=False, mode='w+b') as tmp_file:
             shutil.copyfileobj(response, tmp_file)
 
-    with open(tmp_file.name) as file:
-        coraldefs=pandas.read_csv(file, sep="\t", keep_default_na=False)
-        term=coraldefs.TERM
-        role=coraldefs.ROLE 
-        definition=coraldefs.DEFINITION 
-        referrals=coraldefs.REFERRALS
-        length=len(coraldefs)
-        print(length)
+            with open(tmp_file.name) as file:
+                coraldefs=pandas.read_csv(file, sep="\t", keep_default_na=False)
+                term=coraldefs.TERM
+                role=coraldefs.ROLE 
+                definition=coraldefs.DEFINITION 
+                referrals=coraldefs.REFERRALS
+                length=len(coraldefs) # for jinja loop
+
+        # r=[]
+        # for i in coraldefs.index:
+        #     ref=re.sub(r'(See\salso\b)\s(.*)\.$',r'\1 <a href="\2"> \2 </a>', refs[i])
+        #     r.append(ref) 
+        # df=pandas.DataFrame(r,columns=["refs"])
+
+        # referrals=df.refs
 
 
     return render_template('index.html', coraldefs=coraldefs, term=term, role=role, definition=definition, referrals=referrals)
