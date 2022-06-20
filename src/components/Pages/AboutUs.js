0eeb90dc864076e5eb47ebda53ed531af5626ace
Amanda Ho-Lyn <amandaholyn@gmail.com>
2022-06-20 14:51:44 +0100

    updating about page styling

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -8,18 +8,22 @@ export default function AboutUs() {
       <PageTitle title="About" />
       <Row>
         <Col>
-          Hello! This site was created by Amanda Ho-Lyn in an effort to create a
-          more centralised base for the coral lexicon. She grew up in Jamaica -
-          born to a former diver who ran SunDivers no less! This project is
-          currently looking for contributors and funding. If you're interested,
-          reach out.
-          <br></br>
-          <sub>
-            In future this section will have a more fleshed out description of
-            what the project is and aims to do, with less of a personal touch.
-          </sub>
+          <p className="p-4">
+            Hello! This site was created by Amanda Ho-Lyn in an effort to create
+            a more centralised base for the coral lexicon. She grew up in
+            Jamaica - born to a former diver who ran SunDivers no less! This
+            project is currently looking for contributors and funding. If you're
+            interested, reach out.
+            <br></br>
+            <sub>
+              In future this section will have a more fleshed out description of
+              what the project is and aims to do, with less of a personal touch.
+            </sub>
+          </p>
         </Col>
-        <Col xs={2}>pic</Col>
+        <Col xs={2}>
+          <img src="/public/images/sundivers.jpeg" alt="profile"></img>
+        </Col>
       </Row>
     </Container>
   );
