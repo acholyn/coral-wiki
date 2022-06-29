4c8106c199634f8be5c2570629cd3b6308b0c710
Amanda Ho-Lyn <amandaholyn@gmail.com>
2022-06-29 11:14:46 +0100

    scroll to definition

--- src/components/Pages/Dictionary.js
+++ src/components/Pages/Dictionary.js
@@ -5,22 +5,31 @@ import PageTitle from "../PageTitle";
 import definitions from "../../contents/definitions.json";
 
 export default function Dictionary() {
-  // const defs = definitions.map()
+  const handleAnchorClick = (event) => {
+    let hash = event.target.hash;
+    let anchorElem = document.querySelector(hash);
+    event.preventDefault();
+    anchorElem.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
   return (
     <Container className="Page">
       <PageTitle title="Dictionary" />
       <Container className="dictionary">
         {definitions.map((word, i) => (
-          <p key={i}>
+          <p key={i} id={word.TERM.trim()}>
             <b>{word.TERM}</b> (<i>{word.ROLE}</i>) <br></br>
             {word.DEFINITION}
-            {/* make conditionals for referrals (see also) */}
+            {/* conditionally show referrals */}
             {word.REFERRALS.length >= 2 && (
               <span>
                 {" "}
-                See also <a href={word.REFERRALS}>{word.REFERRALS}</a>
+                See also{" "}
+                <a href={`#${word.REFERRALS}`} onClick={handleAnchorClick}>
+                  {word.REFERRALS}
+                </a>
               </span>
+              // need to sort out 'coined by'
             )}
           </p>
         ))}
