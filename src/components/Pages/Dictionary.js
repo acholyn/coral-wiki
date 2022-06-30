b3bdd199abaea9d6bf3a4331299f07f6418c08a1
Amanda Ho-Lyn <amandaholyn@gmail.com>
2022-06-30 13:32:53 +0100

    updating styles and content

--- src/components/Pages/Dictionary.js
+++ src/components/Pages/Dictionary.js
@@ -13,7 +13,17 @@ export default function Dictionary() {
       linkParentElem.style.color = "red";
     }
     event.preventDefault();
-    anchorElem.scrollIntoView({ behavior: "smooth", block: "start" });
+    anchorElem.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    // animate the word so users know which one it is?
+    // anchorElem.animate([
+    //   { transform: "scale(1.1)" },
+    //   { duration: "1s" },
+    //   { iterations: "1" },
+    //   { delay: "0.6s" },
+    // ]);
   };
 
   return (
@@ -22,18 +32,22 @@ export default function Dictionary() {
       <Container className="dictionary">
         {definitions.map((word, i) => (
           <p key={i} id={word.TERM.trim()}>
-            <b>{word.TERM}</b> (<i>{word.ROLE}</i>) <br></br>
+            <b className="term">{word.TERM}</b> (
+            <i className="role">{word.ROLE}</i>) <br></br>
             {word.DEFINITION}
             {/* conditionally show referrals */}
             {word.REFERRALS.length >= 2 && (
               <span>
                 {" "}
                 See also{" "}
-                <a href={`#${word.REFERRALS}`} onClick={handleAnchorClick}>
+                <a
+                  className="ref-link"
+                  href={`#${word.REFERRALS}`}
+                  onClick={handleAnchorClick}>
                   {word.REFERRALS}
                 </a>
               </span>
-              // need to sort out 'coined by'
+              // need to sort out 'coined by' and
             )}
           </p>
         ))}
